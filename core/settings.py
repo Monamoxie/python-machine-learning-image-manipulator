ba026283288f77ef37dd0ae9cf2aaf222927eec0
Mona Moxie <moxie4lyf@gmail.com>
2024-06-14 20:48:06 +0200

    feat: install django dump die for debugging

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "image",
+    "django_dump_die",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_dump_die.middleware.DumpAndDieMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
