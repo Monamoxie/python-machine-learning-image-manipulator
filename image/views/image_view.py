c85ebb91af5496ffb24988c84fca924ea967c57d
Mona Moxie <moxie4lyf@gmail.com>
2024-06-20 21:04:42 +0200

    wip: file upload form

--- image/views/image_view.py
+++ image/views/image_view.py
@@ -1,9 +1,11 @@
+from django.urls import reverse_lazy
 from django.views import View
 from django.http import HttpResponse
 from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import FormView
 from image.forms import ImageUploadForm
+from django.contrib import messages
 
 
 class ImageListView(View):
@@ -14,7 +16,17 @@ class ImageListView(View):
 class ImageUploadView(FormView):
     form_class = ImageUploadForm
     template_name = "image/index.html"
+    success_url = reverse_lazy("index")
 
     def post(self, request):
-        # WIP
+        form = ImageUploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            pass
+        else:
+            return self.form_invalid(form)
         return render(request, "image/index.html")
+
+    def form_invalid(self, form):
+        messages.error(self.request, form.errors)
+        return super().form_valid(form)
